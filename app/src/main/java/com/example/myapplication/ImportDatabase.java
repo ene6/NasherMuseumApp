b4ae298e234f41808e9c541e04d050c82c9a30d7
Oum Lahade <oumlahade@Oums-MacBook-Pro.local>
2020-10-26 13:17:28 -0400

    shift to better (more oop) structure for searching helper methods, individual searching functions finished (coalescing them is needed)

--- app/src/main/java/com/example/myapplication/ImportDatabase.java
+++ app/src/main/java/com/example/myapplication/ImportDatabase.java
@@ -29,9 +29,11 @@ import java.util.Objects;
 public class ImportDatabase{
 
     private HashMap<String, String[]> infoDict;
+    private HashMap<String, Painting> info;
 
     public ImportDatabase(Context context, String CSV_FILE_PATH){
         infoDict = new HashMap<String, String[]>();
+        info = new HashMap<String, Painting>();
         ArrayList<List<String>> records = new ArrayList<List<String>>();
         try (CSVReader csvReader = new CSVReader(new InputStreamReader(context.getAssets().open(CSV_FILE_PATH)))) {
             String[] values;
@@ -39,11 +41,13 @@ public class ImportDatabase{
                 records.add(Arrays.asList(values));
             }
             for (int i = 0; i < records.size(); i++){
-                int [] a = {2,3,4,5,6,7};
+                int [] a = {1,2,3,4,5,6,7,8};
                 String [] vals = new String [a.length];
                 for (int j = 0 ; j < vals.length; j++)
                     vals[j] = (String) records.get(i).toArray()[a[j]];
+                Log.d("help", new Painting((String)records.get(i).toArray()[0],vals).toString());
                 infoDict.put((String)records.get(i).toArray()[0], vals);
+                info.put((String)records.get(i).toArray()[0], new Painting((String)records.get(i).toArray()[0],vals));
 
             }
             //for (int i = 0 ; i < records.toArray().length)
@@ -65,32 +69,23 @@ public class ImportDatabase{
         return -1;
     }
 
-    public ArrayList<String> searchID(String keyword){ // only for Painting ID, Rack, and
+    public ArrayList<String> searchID(String keyword){ // only for Painting ID, Rack, Location type, Artist, Title
         ArrayList<String> returnVal = new ArrayList<String>();
-        for (String key: infoDict.keySet()){
-            if (key.toLowerCase().trim().contains(keyword.toLowerCase().trim()))
+        for (String key: info.keySet()){
+            if (info.get(key).containsPaintingID(keyword.trim()))
                 returnVal.add(key);
         }
         return returnVal;
     }
 
-    public ArrayList<String> searchLocation(String rackType, String num){
+    public ArrayList<String> searchLocation(String rackType, String rack){
         ArrayList<String> returnVal = new ArrayList<String>();
-        for (String key: infoDict.keySet()){
-            if (infoDict.get(key)[findIndex("Location Type")].toLowerCase().trim().equals(rackType.toLowerCase().trim())){
-                if (num.equals("None"))
+        for (String key: info.keySet()){
+            if (info.get(key).isLocationType(rackType.toLowerCase().trim())){
+                if (rack.equals("None"))
                     returnVal.add(key);
                 else{
-                    try{
-                        Integer.parseInt(num);
-                        if (num.length() <2)
-                            num = '0'+num;
-                    }
-                    catch (NumberFormatException E){
-                        if (num.length() < 3)
-                            num = '0'+num;
-                    }
-                    if (infoDict.get(key)[findIndex("Rack")].toLowerCase().trim().equals(num.toLowerCase().trim()))
+                    if (info.get(key).isRack(rack.toLowerCase().trim()))
                         returnVal.add(key);
                 }
             }
@@ -104,8 +99,17 @@ public class ImportDatabase{
 
     public ArrayList<String> searchTitle(String keyword){
         ArrayList<String> returnVal = new ArrayList<String>();
-        for (String key: infoDict.keySet()){
-            if (infoDict.get(key)[findIndex("Title")].toLowerCase().trim().contains(keyword.toLowerCase().trim()))
+        for (String key: info.keySet()){
+            if (info.get(key).containsTitle(keyword.trim()))
+                returnVal.add(key);
+        }
+        return returnVal;
+    }
+
+    public ArrayList<String> searchArtist(String keyword){
+        ArrayList<String> returnVal = new ArrayList<String>();
+        for (String key: info.keySet()){
+            if (info.get(key).containsArtist(keyword.trim()))
                 returnVal.add(key);
         }
         return returnVal;
--- app/src/main/java/com/example/myapplication/Painting.java
+++ app/src/main/java/com/example/myapplication/Painting.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.util.Log;
+
 public class Painting {
     private String paintingID;
+    private String location;
     private String locationType;
     private String rack;
     private String artist;
@@ -12,17 +15,33 @@ public class Painting {
 
     public Painting(String paintingID, String[] info){
         this.paintingID = paintingID.trim();
-        this.locationType = info[0].trim();
-        this.rack = info[1].trim();
-        this.artist = info[2].trim();
-        this.title = info[3].trim();
-        this.height = info[4].trim();
-        this.width = info[5].trim();
-        this.depth = info[6].trim();
+        this.location = info[0].trim();
+        this.locationType = info[1].trim();
+        this.rack = info[2].trim();
+        this.artist = info[3].trim();
+        this.title = info[4].trim();
+
+        if (info[5].trim().length() > 0)
+            this.height = info[5].trim();
+        else
+            this.height = "None";
+        if (info[6].trim().length() > 0)
+            this.width = info[6].trim();
+        else
+            this.width = "None";
+        if (info[7].trim().length() > 0)
+            this.depth = info[7].trim();
+        else
+            this.depth = "None";
+
     }
 
     public String getPaintingID() {
         return paintingID;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     public String getLocationType() {
@@ -52,4 +71,49 @@ public class Painting {
     public String getDepth() {
         return depth;
     }
+
+    public boolean containsPaintingID(String paintingID){
+        //Log.d("Testing", nasher.searchID("20").toString());
+        return this.paintingID.trim().toLowerCase().contains(paintingID.trim().toLowerCase());
+    }
+
+    public boolean isLocationType(String locationType){
+        return locationType.trim().toLowerCase().equals(this.locationType.trim().toLowerCase());
+    }
+
+    public boolean isRack(String rack){
+        try{
+            Integer.parseInt(rack);
+            if (rack.length() <2)
+                rack = '0'+rack;
+        }
+        catch (NumberFormatException E){
+            if (rack.length() < 3)
+                rack = '0'+rack;
+        }
+
+        return this.rack.toLowerCase().trim().equals(rack.toLowerCase().trim());
+    }
+
+    public boolean containsArtist(String artist){
+        return this.artist.trim().toLowerCase().contains(artist.trim().toLowerCase());
+    }
+
+    public boolean containsTitle(String title){
+        return this.title.trim().toLowerCase().contains(title.trim().toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return "Object: " + paintingID +
+                " is stored at " + location +
+                ".\nIt is on " + locationType + ' ' + rack +
+                ".\tIt has has dimensions as follows\tHeight: " +height + "\tWidth: " + width +
+                "\tDepth: "+ depth;
+    }
 }
+
+/*
+ImportDatabase nasher = new ImportDatabase(this,"nasher_clean_info.csv");
+Log.d("Testing", nasher.searchLocation("Screen","7A").toString());
+        */
