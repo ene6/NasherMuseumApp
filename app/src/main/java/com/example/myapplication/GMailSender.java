4e5f89b58811b33e987392112d703b43f8356fb6
Chris Knotek <chrisknoteksoccer@gmail.com>
2020-10-26 13:08:53 -0400

    added emailing (not working)

---
+++ app/src/main/java/com/example/myapplication/GMailSender.java
@@ -0,0 +1,117 @@
+package com.example.myapplication;
+
+import android.os.StrictMode;
+
+import javax.mail.Authenticator;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Security;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.Message;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GMailSender extends javax.mail.Authenticator {
+    private String mailhost = "smtp.gmail.com";
+    private String user;
+    private String password;
+    private Session session;
+
+    static {
+        Security.addProvider(new com.example.myapplication.JSSEProvider());
+    }
+
+    public GMailSender(String user, String password) {
+        this.user = user;
+        this.password = password;
+
+        Properties props = new Properties();
+
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", mailhost);
+        props.put("mail.smtp.user", user);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        session = Session.getInstance(props, new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("chrisjknotek", "Mythbust3");
+            }
+
+        });
+    }
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(user, password);
+    }
+
+    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception {
+        try{
+            MimeMessage message = new MimeMessage(session);
+            DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));
+            message.setSender(new InternetAddress(sender));
+            message.setSubject(subject);
+            message.setDataHandler(handler);
+            if (recipients.indexOf(',') > 0)
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+            else
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
+            Transport.send(message);
+        }catch(MessagingException mex){
+            System.out.println("Error: unable to send message....");
+            mex.printStackTrace();
+        }
+    }
+
+    public class ByteArrayDataSource implements DataSource {
+        private byte[] data;
+        private String type;
+
+        public ByteArrayDataSource(byte[] data, String type) {
+            super();
+            this.data = data;
+            this.type = type;
+        }
+
+        public ByteArrayDataSource(byte[] data) {
+            super();
+            this.data = data;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getContentType() {
+            if (type == null)
+                return "application/octet-stream";
+            else
+                return type;
+        }
+
+        public InputStream getInputStream() throws IOException {
+            return new ByteArrayInputStream(data);
+        }
+
+        public String getName() {
+            return "ByteArrayDataSource";
+        }
+
+        public OutputStream getOutputStream() throws IOException {
+            throw new IOException("Not Supported");
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/SubmissionActivity.java
+++ app/src/main/java/com/example/myapplication/SubmissionActivity.java
@@ -2,13 +2,72 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class SubmissionActivity extends AppCompatActivity {
+public class SubmissionActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+        final Button send = (Button) this.findViewById(R.id.submitBtn);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new MyTask().execute("This is the email message");
+            }
+        });
     }
+
+    private class MyTask extends AsyncTask<String, Integer, String> {
+
+
+        //Run in UI before background thread called
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        //runs in background
+        @Override
+        protected String doInBackground(String... params) {
+            //get string from params
+            String message = params[0];
+
+            /* Start of sending email */
+
+            try {
+                GMailSender sender = new GMailSender("nasherstorage", "EGR101F20");
+                sender.sendMail("This is Subject",
+                        message,
+                        "nasherstorage",
+                        "chrisjknotek@gmail.com");
+                Log.i("Send Mail", "Email Sent");
+            } catch (Exception e) {
+                Log.e("SendMail", e.getMessage(), e);
+            }
+
+
+            /* End of sending email */
+            return "postExe";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+
+    }
+
 }
